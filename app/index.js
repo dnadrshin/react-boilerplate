621ee1ec663e158a6a2d09a095cd6fdc4ab57e12
dnadrshin <dimankaliningrad@gmail.com>
2017-05-21 23:27:41 +0200

    add redux

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Switch, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 import { createStore } from 'redux'
 import Settings from './settings'
 import Chat from './chat'
@@ -10,18 +11,35 @@ import Github from './github'
 let someData = [1,2,3,4,5],
 	GithubList = () => <Github list={someData}/>
 
+
+var reducer = (state = 1, action) => {
+	switch (action.type) {
+		case "INCREASE":
+			return state + 1
+		case "DECREASE":
+			return state - 1
+		default:
+			return state
+	}
+}
+
+var store = createStore(reducer)
+
+
 ReactDOM.render(
-	<BrowserRouter>
-		<div>
-			<Menu/>
-			<Switch>
-				<Route exact path="/" component={Settings} />
-				<Route path="/settings" component={Settings} />
-				<Route path="/chat" component={Chat} />
-				<Route path="/github" component={GithubList} />
-			</Switch>
-		</div>
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Menu/>
+				<Switch>
+					<Route exact path="/" component={Settings} />
+					<Route path="/settings" component={Settings} />
+					<Route path="/chat" component={Chat} />
+					<Route path="/github" component={GithubList} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>,
 
 	document.getElementById('root')
 );
--- app/settings.js
+++ app/settings.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-const Settings = props => <div>
-	<h1>Settings</h1>
-	<Link to={'/'}>Back</Link>
-</div>
+class Settings extends React.Component {
+	constructor() {
+		super()
+	}
+	render(){
+		return <div>
+			<h1>Settings {this.props.state}</h1>
+			<button onClick={this.props.increase}>+</button>
+			<button onClick={this.props.decrease}>-</button>
+		</div>
+	}
+}
 
-
-
-export default Settings
+export default connect(
+	state => ({
+		state: state
+	}),
+	dispatch => ({
+		increase: () => {dispatch({type:'INCREASE'})},
+		decrease: () => {dispatch({type:'DECREASE'})}
+	})
+)(Settings)
