e9bb7796a341418304ea9773bcc03846a023bc99
dnadrshin <dimankaliningrad@gmail.com>
2017-05-28 15:37:43 +0200

    fix fetch

--- app/sagas.js
+++ app/sagas.js
@@ -1,14 +1,14 @@
 import {call, put, takeEvery} from 'redux-saga/effects'
 
 const
-	settingsResp = fetch('http://localhost:4000/API/', {})
+	settingsResp = () => fetch('http://localhost:4000/API/', {})
 	.then(res => res.json())
 	.then(data => data)
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchSettings() {
 	try {
-		const settings = yield call(() => settingsResp);
+		const settings = yield call(settingsResp);
 		yield put({id: settings.number, type: 'SET'});
 	} catch(e) {
 		yield put({message: e.message, type: 'SETTINGS_FETCH_FAILED'});
